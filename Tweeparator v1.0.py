b9b320c0af77c33c9a515b0311aa942a3138d6c8
manaskocharekar <kocharekarmanas@gmail.com>
2017-12-05 20:04:44 +0530

    .

--- Tweeparator v1.0.py
+++ Tweeparator v1.0.py
@@ -6,10 +6,10 @@ __author__ = '@ManasKocharekar'
 
 
 #KEYS
-CUST_KEY = 'CUST_KEY'
-CUST_SECRET = 'CUST_SECRET'
-APP_KEY = 'APP_TOKEN'
-APP_SECRET = 'APP_SECRET'
+CUST_KEY = '7TfeHq5DOg5zidfSTbP15mL4L'
+CUST_SECRET = 'wkn03374J04tbgqoE8UemjfjfXJBKy4fQM8G8jFpuNd4nhrDKc'
+APP_KEY = '936617132963463168-0kTv9jK0kAxougV5rQlHIMJOos1zTLK'
+APP_SECRET = 'GaspSZqoO6sA9Q7W9YBYQIZDRSTFcOItcaZ8sdBWLk19r'
 auth = tweepy.OAuthHandler(CUST_KEY, CUST_SECRET)
 auth.set_access_token(APP_KEY, APP_SECRET)
 
@@ -26,7 +26,7 @@ else:
 
 print(TweetText)
 
-if len(TweetText) <= 140:
+if len(TweetText) <= 280:
     access.update_status(TweetText)
     print('Tweeted Successfully')
 else:
