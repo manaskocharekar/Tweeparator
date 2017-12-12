b9e192719c35ec2365d6a8fe88dd79e313ebcdec
manaskocharekar <kocharekarmanas@gmail.com>
2017-12-12 07:16:40 +0530

    minor fix

--- Tweeparator v1.0.py
+++ Tweeparator v1.0.py
@@ -7,25 +7,28 @@ from nltk.tokenize import sent_tokenize
 __author__ = '@ManasKocharekar'
 
 #KEYS
-CUST_KEY = 'CUST_KEY'
-CUST_SECRET = 'CUST_SECRET'
-APP_KEY = 'APP_TOKEN'
-APP_SECRET = 'APP_SECRET'
-auth = tweepy.OAuthHandler(CUST_KEY, CUST_SECRET)
-auth.set_access_token(APP_KEY, APP_SECRET)
-
-access = tweepy.API(auth)
-
+##CUST_KEY = 'CUST_KEY'
+##CUST_SECRET = 'CUST_SECRET'
+##APP_KEY = 'APP_TOKEN'
+##APP_SECRET = 'APP_SECRET'
+##auth = tweepy.OAuthHandler(CUST_KEY, CUST_SECRET)
+##auth.set_access_token(APP_KEY, APP_SECRET)
+##
+##access = tweepy.API(auth)
+##
 TweetThis = str(sys.argv[1])
 
 filename = open(TweetThis, 'r')
 TweetText = filename.read()
 filename.close()
 
-print(TweetText)
-
-if len(TweetText) <= 280:
-    access.update_status(TweetText)
-    print('Tweeted Successfully')
-else:
-    print('Tweet Length Exceeded')
+for i in sent_tokenize(TweetText):
+    print(i)
+    print()
+    print()
+##
+##if len(TweetText) <= 280:
+##    access.update_status(TweetText)
+##    print('Tweeted Successfully')
+##else:
+##    print('Tweet Length Exceeded')
